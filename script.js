3d916a215d65b8f784d79dbd7f0af8c7a0501aac
Maha561 <mahalakshmianandan04@gmail.com>
2025-07-05 21:06:53 +0530

    added

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 
-let lightBtn = document.getElementById("lightBtn")
+let lightBtn = document.getElementById("lightBtn");
+
+let darkBtn = document.getElementById("darkBtn");
+
+lightBtn.addEventListener("click",function(){
+    document.body.style.backgroundColor="#f3e5f5";
+    document.body.style.color="black";
+});
+
+darkBtn.addEventListener("click",function(){
+    document.body.style.backgroundColor="#1c1c1c";
+    document.body.style.color="white";
+});
